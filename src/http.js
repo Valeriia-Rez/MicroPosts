fff52ae8509c7cf0677cb9a818cc8756f99a8f81
Valeriia-Rez <Valeria09122016@gmail.com>
2020-02-08 15:33:53 +0200

    Post Edit State and Update

--- src/http.js
+++ src/http.js
@@ -20,7 +20,10 @@ class EasyHTTP {
     async put(url, data) {
         const response = await fetch(url, {
             method: "PUT",
-            headers: JSON.stringify(data)
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
         });
         const resData = await response.json();
         return resData;
--- src/ui.js
+++ src/ui.js
@@ -16,7 +16,7 @@ class UI {
                 <div class="card-body">
                 <h4 class="card-text">${post.title}</h4>
                 <p class ="card-text">${post.body}</p>
-                <a href="#" class="edit card-link data-id"${post.id}">
+                <a href="#" class="edit card-link" data-id="${post.id}">
                     <i class="fa fa-pencil"></i>
                 </a>
                 <a href="#" class="delete card-link" data-id="${post.id}">
@@ -61,6 +61,53 @@ class UI {
     clearFields() {
         this.titleInput.value = "",
             this.bodyInput.value = ""
+    }
+
+    //Fill form to edit
+    fillForm(data) {
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.idInput.value = data.id;
+
+        this.changeFormState("edit");
+    }
+
+    //Clear ID hidden value
+    clearIdInput() {
+        this.idInput.value = "";
+    }
+
+    //Change the form state
+    changeFormState(type) {
+        if (type === "edit") {
+            this.postSubmit.textContent = "Update Post";
+            this.postSubmit.className = "post-submit btn btn-warning btn-block";
+
+            //Create cancel button
+            const button = document.createElement("button");
+            button.className = "post-cancel btn btn-light btn-block";
+            button.appendChild(document.createTextNode("Cancel Edit"));
+
+            //Get parent
+            const cardForm = document.querySelector(".card-form");
+
+            //Get element to insert before
+            const formEnd = document.querySelector(".form-end");
+
+            //Insert cancel button
+            cardForm.insertBefore(button, formEnd);
+        } else {
+            this.postSubmit.textContent = "Post it";
+            this.postSubmit.className = "post-submit btn btn-primary btn-block";
+            //Remove cancel btn if it is there
+            if (document.querySelector(".post-cancel")) {
+                document.querySelector(".post-cancel").remove();
+            }
+            //Clear ID from hidden field
+            this.clearIdInput();
+            //Clear Text
+            this.clearFields();
+        }
     }
 }
 
